2b66694572b52a51de71b392f7bd03eae1b0710f
unknown <johanes.steven@ui.ac.id>
2022-09-30 23:13:24 +1000

    add clock and date to the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Image from 'next/image';
 import { Typography } from '@mui/material';
 import { Container } from '@mui/system';
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import Clock from './clock';
+
+import "@fontsource/open-sans";
+import "@fontsource/mohave";
 
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Open Sans',
+      'Mohave',
+      'sans-serif',
+    ].join(','),
+    body1: {
+      fontWeight: 700,
+    },
+  },
+});
 
 const Home: NextPage = () => {
   return (
@@ -16,19 +34,22 @@ const Home: NextPage = () => {
     <Container>
       <Grid container justifyContent='flex-end'>
         <Grid item style={{marginTop:"2%"}}>
-          <Typography>Brisbane, {date}</Typography>
+          <ThemeProvider theme={theme}>
+            <Typography fontFamily='Open Sans'>Brisbane, {date}</Typography>
+            <Clock />
+          </ThemeProvider>
         </Grid>
       </Grid>
 
         <Grid container direction="column" justifyContent="center" alignItems="center" spacing = {2}>
 
-          <Grid item style={{marginTop: "10%", marginBottom:"5%"}}>
-            <Image src="/logo.png" width={"850%"} height={"200%"} />
+          <Grid item style={{marginTop: "2%", marginBottom:"5%"}}>
+            <Image src="/logo.png" width={"680%"} height={"145%"} />
           </Grid>
           
           <Grid item>
             <Link href={`\create`}>
-              <Button variant="contained" href="#contained-buttons">
+              <Button variant="contained" href="#contained-buttons" fullWidth={true}>
                 Create an Event 
               </Button>
             </Link>
@@ -36,9 +57,14 @@ const Home: NextPage = () => {
           <Grid item>
             <Link href={`\event`}>
               <Button variant="contained" href="#contained-buttons">
-                Create an Event 
+                See Events
               </Button>
             </Link>
+          </Grid>
+
+          <Grid item style={{marginTop: "2%"}}>
+            <hr style={{color:"#784CF4", backgroundColor:"#784CF4"}}/>
+            <Typography fontFamily={'Mohave'} fontSize={'2.2rem'} style= {{color:"#784CF4", fontWeight:"bold"}}>UPCOMING EVENTS</Typography>
           </Grid>
         </Grid>
       </Container>
