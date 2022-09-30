356e546ac12b9bcf3c7e18239446e66673554cab
unknown <jarfix01@gmail.com>
2022-09-30 21:00:24 +1000

    feat: added task addition functionality

--- src/components/DateTimeInput.tsx
+++ src/components/DateTimeInput.tsx
@@ -5,7 +5,30 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { Grid, Box } from '@mui/material';
+import { Grid } from '@mui/material';
+import { withStyles } from '@material-ui/core/styles';
+
+const CssTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: "#784CF4",
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: "#784CF4",
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: "#784CF4",
+        },
+        '&:hover fieldset': {
+          borderColor: "#784CF4",
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: "#784CF4",
+        },
+      },
+    },
+  })(TextField);
 
 export default function BasicDatePicker() {
     const [date, setDate] = React.useState<Dayjs | null>(null);
@@ -20,16 +43,16 @@ export default function BasicDatePicker() {
             onChange={(newValue) => {
                 setDate(newValue);
             }}
-            renderInput={(params) => <TextField focused color="secondary" {...params} />}
+            renderInput={(params) => <CssTextField focused {...params} />}
             />        
         </Grid>
         <Grid item xs={6}>
-        <TimePicker
-            label="Time"
-            value={time}
-            onChange={setTime}
-            renderInput={(params) => <TextField focused color="secondary" {...params} />}
-        />
+            <TimePicker
+                label="Time"
+                value={time}
+                onChange={setTime}
+                renderInput={(params) => <CssTextField focused {...params} />}
+            />
         </Grid>
         </Grid>
     </LocalizationProvider>
--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -9,11 +9,69 @@ import Dropzone from '../../components/Dropzone';
 import BasicDatePicker from '../../components/DateTimeInput';
 import FreeSolo from '../../components/AutoComplete';
 import Chip from '@mui/material/Chip';
+import { withStyles } from '@material-ui/core/styles';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: "#784CF4"
+    }
+  },
+});
+
+const CssTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: "#784CF4",
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: "#784CF4",
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: "#784CF4",
+      },
+      '&:hover fieldset': {
+        borderColor: "#784CF4",
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: "#784CF4",
+      },
+    },
+  },
+})(TextField);
 
 const Create: NextPage = () => {
+  const [hashtag, setHashtag] = React.useState("")
+  const [arrayOfHashtags, addHashtag] = React.useState([]);
+
+  const handleDelete = (h) => () => {
+    addHashtag((arrayOfHashtags) =>
+      arrayOfHashtags.filter((hashtag) => hashtag !== h)
+    )
+  }
+
+  const newHashtag = () => {
+    addHashtag((arrayOfHashtags) => arrayOfHashtags.concat(hashtag))
+    console.log(arrayOfHashtags)
+  } 
+
+  const Hashtags = arrayOfHashtags.map((h) => (
+    <Grid item xs={4} sm={2}>
+      <Chip
+        size="medium"
+        label={h}
+        onDelete={handleDelete(h)}
+      />
+    </Grid>
+  ))
 
   return (
-    <Container style={{paddingTop:"5%"}}>
+    <Container style={{marginTop:"5%", marginBottom:"5%"}}>
       <Grid container alignItems='center' justifyContent='space-between' style={{marginBottom:"5%"}}>
         <Grid item spacing={2}>
           <ArrowBackIosNewIcon/>
@@ -30,42 +88,21 @@ const Create: NextPage = () => {
 
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <TextField
-            color="secondary" 
-            focused
-            id="title"
-            label="Title"
-            fullWidth
-          />
+          <CssTextField label="Title" focused fullWidth/>
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            color="secondary"
-            focused
-            id="description"
-            label="Description"
-            multiline
-            fullWidth
-            rows={4}
-            defaultValue=""
-          /> 
+          <CssTextField label="Description" focused rows={5} multiline fullWidth/>
         </Grid> 
         <Grid item xs={12} sm={6}>
-          <TextField
-            color="secondary"
-            focused
-            id="location"
-            label="Location"
-            fullWidth
-          />
+          <CssTextField label="Location" focused fullWidth/>
         </Grid>
         <Grid item xs={12} sm={6}>
           <BasicDatePicker/>
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={1}>
-            <Grid item xs={4} sm={2}>
-              <Chip label="MASAK AIR BIAR MATENG" onDelete={() => {}} style={{width:"100%"}} /> 
+            {/* <Grid item xs={4} sm={2}>
+              <Chip label="MASAK AIR BIAR MATENG" id="chip1" onDelete={handleDelete} style={{width:"100%"}} /> 
             </Grid>
             <Grid item xs={4} sm={2}>
               <Chip label="ABE GANTENG" onDelete={() => {}} style={{width:"100%"}} /> 
@@ -74,27 +111,27 @@ const Create: NextPage = () => {
               <Chip label="SYASYA CAKEP" onDelete={() => {}} style={{width:"100%"}} /> 
             </Grid>
             <Grid item xs={4} sm={2}>
-              <Chip label="JO DERAS" onDelete={() => {}} style={{width:"100%"}} /> 
-            </Grid>
-          </Grid>
+              <Chip label="JO DERAS Spending" onDelete={() => {}} style={{width:"100%"}} /> 
+            </Grid> */}
+          {arrayOfHashtags.length > 0 ? Hashtags : ""}
+          </Grid> 
+        </Grid>
+        <Grid item xs={9}>
+          <FreeSolo value={hashtag} onChange={(e) => {setHashtag(e.target.value)}}/> 
+        </Grid>
+        <Grid item xs={3} style={{color:"#784CF4"}}>
+          <AddCircleOutlineIcon fontSize="large" onClick={newHashtag}/>
         </Grid>
         <Grid item xs={12}>
-          <FreeSolo/> 
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            color="secondary"
-            focused
-            id="recipe"
-            label="Recipe"
-            multiline
-            fullWidth
-            rows={4}
-            defaultValue=""
-          />    
+          <CssTextField label="Recipe" focused fullWidth multiline rows={5}/>
         </Grid>
         <Grid item xs={12}>
           <Dropzone/>
+        </Grid>
+        <Grid item xs={12}>
+        <ThemeProvider theme={theme}>
+          <Button variant="contained" color='primary'>Contained</Button>
+        </ThemeProvider>
         </Grid>
       </Grid>
     </Container>
