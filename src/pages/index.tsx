d355aa0aa7a04d857390ffad26f90e3dda69246d
unknown <johanes.steven@ui.ac.id>
2022-09-30 19:24:35 +1000

    add landing page with logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,29 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Image from 'next/image';
+import { Typography } from '@mui/material';
+import { Container } from '@mui/system';
+
+const current = new Date();
+const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Student Accommodation
-        </h1>
 
-        <p className={styles.description}>
-          Weekly Cooking Events
-        </p>
+    <Container>
+      <Grid container justifyContent='flex-end'>
+        <Grid item style={{marginTop:"2%"}}>
+          <Typography>Brisbane, {date}</Typography>
+        </Grid>
+      </Grid>
 
         <Grid container direction="column" justifyContent="center" alignItems="center" spacing = {2}>
+
+          <Grid item style={{marginTop: "10%", marginBottom:"5%"}}>
+            <Image src="/logo.png" width={"850%"} height={"200%"} />
+          </Grid>
+          
           <Grid item>
             <Link href={`\create`}>
               <Button variant="contained" href="#contained-buttons">
@@ -32,16 +41,7 @@ const Home: NextPage = () => {
             </Link>
           </Grid>
         </Grid>
-
-        {/* <div className={styles.grid}>
-          
-        </div> */}
-      </main>
-
-      <footer className={styles.footer}>
-        {/* <p>This is the footer</p> */}
-      </footer>
-    </div>
+      </Container>
   )
 }
 
