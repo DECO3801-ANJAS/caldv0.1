f86a94d0dff07959fa296b5ecf389aecbc796251
bbdyupup <1572974844@qq.com>
2022-10-11 00:09:21 +1000

    remove useless test code e Please enter the commit message for your changes. Lines starting

--- src/pages/api/events/[event_id].ts
+++ src/pages/api/events/[event_id].ts
@@ -1,13 +1,11 @@
 import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
-import useSWR from "swr";
-import { useRouter } from "next/router";
 import IEvent from "../../../interfaces/models/event";
 import IErrorResponse from "../../../interfaces/responses/error";
 import IEventResponse from "../../../interfaces/responses/eventResponse";
 import clientPromise from "../../../lib/mongodb";
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IErrorResponse | IEventResponse>
@@ -17,10 +15,7 @@ export default async function handler(
     const _id = new ObjectId(event_id as string);
     const client = await clientPromise;
     const db = client.db(process.env.DB_NAME);
-    const router = useRouter();
-    const { data: responseDetail, error } = useSWR(router.isReady ? `/event?id=${event_id}` : null,
-    fetcher, { refreshInterval: 10000 }
-  )
+
     const event = (await db.collection("events").findOne({ _id })) as IEvent;
 
     if (!event) {
