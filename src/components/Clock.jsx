92d7d3d073cb5b1f2f67eb645c20ed373bc17238
unknown <johanes.steven@ui.ac.id>
2022-10-12 02:41:04 +1000

    feat: add fetching upcoming events based on date

---
+++ src/components/Clock.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@mui/material';
+import "@fontsource/open-sans";
+
+function Clock() {
+     const[clockState, setClockState] = useState();
+
+     useEffect(() => {
+          setInterval(() => {
+               const date = new Date();
+               setClockState(date.toLocaleTimeString());
+          }, 1000);
+          // return () => {
+          //      clearInterval(interval);
+          // }
+     }, []);
+     
+     return (<Typography fontFamily='Open Sans'>{clockState}</Typography>);
+}
+export default Clock;
