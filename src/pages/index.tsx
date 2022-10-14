a001ce69ce36639940699805b42d7ad86a4b1445
unknown <jarfix01@gmail.com>
2022-10-14 21:40:31 +1000

    refactor: changed font family for upcoming events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,9 +17,8 @@ import IEvent from "../interfaces/models/event";
 import useSWR from "swr";
 
 const current = new Date();
-const date = `${current.getDate()}/${
-  current.getMonth() + 1
-}/${current.getFullYear()}`;
+const date = `${current.getDate()}/${current.getMonth() + 1
+  }/${current.getFullYear()}`;
 
 const theme = createTheme({
   typography: {
@@ -110,7 +109,7 @@ const Home: NextPage = () => {
                   imgAlt={upcomingEvent.title}
                 />
               ) : (
-                <Typography fontFamily="Open Sans">
+                <Typography fontFamily="Mohave">
                   NO UPCOMING EVENTS
                 </Typography>
               )}
