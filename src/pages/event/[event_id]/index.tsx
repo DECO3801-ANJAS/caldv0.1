f3969df9e5b586be17dbb58f8eba4d0c9ac4cd45
unknown <jarfix01@gmail.com>
2022-10-16 21:00:45 +1000

    refactor : changed the layout of the event detail page

--- src/pages/event/[event_id]/index.tsx
+++ src/pages/event/[event_id]/index.tsx
@@ -47,90 +47,118 @@ const EventDetail: NextPage = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-      <Grid container justifyContent='space-between' style={{ padding: "1rem" }}>
-        <Grid item xs={6}>
-          <ArrowBack href={"/event/"}/>
-        </Grid>
-        <Grid item xs={6} sx={{ textAlign: "right" }}>
+        <Grid container justifyContent='space-between' style={{ padding: "1rem" }}>
+          <Grid item xs={6}>
+            <ArrowBack href={"/event/"} />
+          </Grid>
+          <Grid item xs={6} sx={{ textAlign: "right" }}>
             <Dates />
             <Clock />
           </Grid>
         </Grid>
 
-      <Grid container style={isXXS ? {marginBottom:"9rem"} : {marginBottom: "3rem"}}>
-      {!!eventData.data ? (
-        <>
-        <Grid item xs={12} sm={6} style={{ padding: "1rem" }}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Image src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp" layout='responsive'
-              width={16} height={16} quality={65} sizes={"20(max-width: 768px) 100vw,(max-width: 1200px) 50vw, 33vw"} alt='' />
-          </Grid>
-          <Grid item xs={12} sx={{padding:"0.5rem"}}>
-            <Grid container justifyContent={"center"}>
-              <Link href={`/event/${router.query.event_id}/recipe`}>
-                <Button variant='contained'>View Recipe</Button>
-              </Link>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} sm={6} style={{ padding: "1rem" }}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Typography
-              fontFamily="Open Sans"
-              variant="h4"
-              component="h1"
-              gutterBottom
-              sx={{ textTransform: "capitalize", fontWeight: "600", overflowY: 'hidden' }}
-            >
-              {eventData.data.event.title}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Grid container direction={"column"}>
-              <Typography fontFamily="Open Sans">Location:</Typography><Typography color={"#784CF4"}> {eventData.data.event.location}</Typography>
-              <Typography fontFamily="Open Sans">Time: </Typography><Typography color={"#784CF4"}>{new Date(eventData.data.event.date).toLocaleString()}</Typography>
-              <Typography fontFamily="Open Sans">Tasks: </Typography><Typography color={"#784CF4"}>{eventData.data.event.tasks.join(', ')}</Typography>
-              <Typography fontFamily="Open Sans">Description:</Typography><Typography color={"#784CF4"}>
-                {eventData.data.event.description.split('\n').map((str:string, i:number) => <p key={i}>{str}</p>)}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      </>
-        ) : (
-          <Grid item xs={12}>
+        <Grid container style={isXXS ? { marginBottom: "9rem" } : { marginBottom: "3rem" }}>
+          {!!eventData.data ? (
+            <>
+              <Grid item xs={12} sm={6} style={{ padding: "1rem" }}>
+                <Grid container>
+                  <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <Image src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp" width={500} height={500} alt='' />
+                  </Grid>
+                  <Grid item xs={12} sx={{ padding: "0.5rem" }}>
+                    <Grid container justifyContent={"center"}>
+                      <Link href={`/event/${router.query.event_id}/recipe`}>
+                        <Button variant='contained'>View Recipe</Button>
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} sm={6} style={{ padding: "1rem" }}>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <Typography
+                      fontFamily="Open Sans"
+                      variant="h4"
+                      component="h1"
+                      gutterBottom
+                      sx={{ textTransform: "capitalize", fontWeight: "600", overflowY: 'hidden' }}
+                    >
+                      {eventData.data.event.title}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} display={isXXS ? "none" : "block"}>
+                    <Grid container>
+                      <Grid item xs={12} textAlign={isXXS ? "center" : "left"}>
+                        <Typography fontFamily="Open Sans">{!!participantData.data ? participantData.data.participants.length : "0"} JOINING</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Grid container>
+                          <Grid xs={12} sm={3} item style={isXXS ? { padding: "0.5rem 0.5rem" } : { padding: "0.5rem 0rem" }}>
+                            <Link href={`/event/${router.query.event_id}/participants`}>
+                              <Button variant="outlined" fullWidth={isXXS} color="primary">View Participants</Button>
+                            </Link>
+                          </Grid>
+                          <Grid xs={12} sm={3} item style={isXXS ? { padding: "0.5rem 0.5rem" } : { padding: "0.5rem 0rem" }}>
+                            <Link href={`/event/${router.query.event_id}/join`}>
+                              <Button variant="contained" fullWidth={isXXS} color="primary">Join</Button>
+                            </Link>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid container direction={"column"}>
+                      <Typography fontFamily="Open Sans">Location:</Typography><Typography color={"#784CF4"}> {eventData.data.event.location}</Typography>
+                      <Typography fontFamily="Open Sans">Time: </Typography><Typography color={"#784CF4"}>{new Date(eventData.data.event.date).toLocaleString()}</Typography>
+                      <Typography fontFamily="Open Sans">Tasks: </Typography><Typography color={"#784CF4"}>{eventData.data.event.tasks.join(', ')}</Typography>
+                      <Typography fontFamily="Open Sans">Description:</Typography><Typography color={"#784CF4"}>
+                        {eventData.data.event.description.split('\n').map((str: string, i: number) => <p key={i}>{str}</p>)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          ) : (
+            <Grid item xs={12}>
               <Grid container justifyContent={"center"}>
                 <CircularProgress />
               </Grid>
             </Grid>
-        )}
-      </Grid>
+          )}
+        </Grid>
 
-      <Box sx={{ backgroundColor:"white", position: 'fixed', bottom: 0, left: 0, right: 0, padding:"0.5rem", borderTop:"solid 1px #784CF4"}}>
-        <Grid container justifyContent={"space-between"} alignItems={"center"}>
-          <Grid item xs={12} sm={6} style={{textAlign:"center"}}>
-            <Typography fontFamily="Open Sans">{!!participantData.data? participantData.data.participants.length : "0"} JOINING</Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Grid container justifyContent="center">
-              <Grid xs={12} sm={"auto"} item style={isXXS ? {padding:"0.5rem 0.5rem"} : {padding:"0rem 0.5rem"}}>
-                <Link href={`/event/${router.query.event_id}/participants`}>
-                  <Button variant="outlined" fullWidth={isXXS} color="primary">View Participants</Button>
-                </Link>
-              </Grid>
-              <Grid xs={12} sm={"auto"} item style={isXXS ? {padding:"0.5rem 0.5rem"} : {padding:"0rem 0.5rem"}}>
-                <Link href={`/event/${router.query.event_id}/join`}>
-                  <Button variant="contained" fullWidth={isXXS} color="primary">Join</Button>
-                </Link>
+        <Box display={isXXS ? "block" : "none"} sx={{
+          backgroundColor: "white",
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          padding: "0.5rem",
+          borderTop: "solid 1px #784CF4"
+        }}>
+          <Grid container justifyContent={"space-between"} alignItems={"center"}>
+            <Grid item xs={12} sm={6} style={{ textAlign: "center" }}>
+              <Typography fontFamily="Open Sans">{!!participantData.data ? participantData.data.participants.length : "0"} JOINING</Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Grid container justifyContent="center">
+                <Grid xs={12} sm={"auto"} item style={isXXS ? { padding: "0.5rem 0.5rem" } : { padding: "0rem 0.5rem" }}>
+                  <Link href={`/event/${router.query.event_id}/participants`}>
+                    <Button variant="outlined" fullWidth={isXXS} color="primary">View Participants</Button>
+                  </Link>
+                </Grid>
+                <Grid xs={12} sm={"auto"} item style={isXXS ? { padding: "0.5rem 0.5rem" } : { padding: "0rem 0.5rem" }}>
+                  <Link href={`/event/${router.query.event_id}/join`}>
+                    <Button variant="contained" fullWidth={isXXS} color="primary">Join</Button>
+                  </Link>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
       </ThemeProvider>
     </>
   );
